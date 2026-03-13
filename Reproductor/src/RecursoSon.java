c7127b384cbed46ea45cead17e3ea32bf2ac73bf
RicherVG <166271618+RicherVG@users.noreply.github.com>
2026-03-13 09:04:02 -0600

    subida de interface y enum y creacion de controlmusical

---
+++ Reproductor/src/RecursoSon.java
@@ -0,0 +1,58 @@
+
+import java.io.File;
+import java.io.Serializable;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author riche
+ */
+public abstract class RecursoSon implements Serializable {
+    private static final long serialVersionUID = 1L;
+    protected String tituloTema;
+    protected String autorTema;
+    protected String tiempoTexto;
+    protected String direccionAudio;
+    protected String direccionPortada;
+    protected TipoGenero tipoGenero;
+    public RecursoSon(String tituloTema, String autorTema,String tiempoTexto,String direccionAudio,String direccionPortada,TipoGenero tipoGenero) {
+        this.tituloTema = tituloTema;
+        this.autorTema = autorTema;
+        this.tiempoTexto = tiempoTexto;
+        this.direccionAudio = direccionAudio;
+        this.direccionPortada = direccionPortada;
+        this.tipoGenero = tipoGenero;
+    }
+
+    public abstract String obtenerCategoria();
+    public String getTituloTema() {
+        return tituloTema;
+    }
+    public String getAutorTema() {
+        return autorTema;
+    }
+    public String getTiempoTexto() {
+        return tiempoTexto;
+    }
+    public String getDireccionAudio() {
+        return direccionAudio;
+    }
+    public String getDireccionPortada() {
+        return direccionPortada;
+    }
+    public TipoGenero getTipoGenero() {
+        return tipoGenero;
+    }
+    public File getArchivoAudio() {
+        return new File(direccionAudio);
+    }
+    @Override
+    public String toString() {
+        return tituloTema + " - " + autorTema;
+    }
+
+}
---
+++ Reproductor/src/TipoGenero.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+/**
+ *
+ * @author riche
+ */
+public enum TipoGenero {
+    POP,
+    ROCK,
+    REGGAETON,
+    ELECTRONICA,
+    CLASICA,
+    JAZZ,
+    ROMANTICA,
+    CRISTIANA,
+    INSTRUMENTAL,
+    OTRO
+}
